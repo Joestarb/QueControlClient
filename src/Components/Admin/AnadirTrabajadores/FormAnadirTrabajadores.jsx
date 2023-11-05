052c272b779a27da3e6001999e0ae4bead9ebd30
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-11-05 10:53:49 -0500

    se agrego usuarios

--- src/Components/Admin/AnadirTrabajadores/FormAnadirTrabajadores.jsx
+++ src/Components/Admin/AnadirTrabajadores/FormAnadirTrabajadores.jsx
@@ -1,34 +1,132 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useState } from 'react';
 
 function FormAnadirTrabajadores() {
-    return (
-        <div className='grid place-content-center'>
+  const [userData, setUserData] = useState({
+    nombre: '',
+    contrasenia: '',
+    perfil: '',
+    telefono: '',
+    infoAdicional: '',
+    estatus: true, // Puedes establecer un valor predeterminado
+  });
 
-            <div className='bg-[#AEC5FF]  p-5 w-full'>
-                <h1 className=' text-white'>Añadir Trabajadores</h1>
-            </div>
-            <form className='flex flex-col bg-white border border-3 pt-3 pb-10 px-28 h-full w-full'>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
 
-                <label className='my-1' htmlFor="nombre">Nombre de Trabajador: </label><input className='   w-80 rounded-lg p-1 border-gray-400   border-2 ' type="text" name="nombre" />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-                <label className='my-1' htmlFor="Contraseña">Contraseña: </label><input className='   w-80 rounded-lg p-1  border-gray-400   border-2 ' type="password" name="Contraseña" />
+    // Validar que el campo "estatus" no esté en blanco
+    if (!userData.estatus) {
+      alert('El campo "estatus" no puede estar en blanco');
+      return;
+    }
 
-                <label className='my-1' htmlFor="Contraseña">Confirmar Contraseña: </label><input className='   w-80 rounded-lg p-1  border-gray-400   border-2 ' type="password" name="Contraseña" />
+    try {
+      const response = await Axios.post('http://localhost:3000/usuarios', userData);
+      // Manejar la respuesta aquí si es necesario
+      console.log(response.data);
+    } catch (error) {
+      // Manejar errores aquí
+      console.error(error);
+    }
+  };
 
-                <label className='my-1' htmlFor="Disponibilidad">Disponibilidad: </label><input className='   w-80 rounded-lg p-1  border-gray-400   border-2 ' type="text" name="Disponibilidad" />
+  return (
+    <div className='grid place-content-center'>
+      <div className='bg-[#AEC5FF]  p-5 w-full'>
+        <h1 className='text-white'>Añadir Trabajadores</h1>
+      </div>
+      <form
+        className='flex flex-col bg-white border border-3 pt-3 pb-10 px-28 h-full w-full'
+        onSubmit={handleSubmit}
+      >
+        <label className='my-1' htmlFor="nombre">
+          Nombre de Trabajador:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="text"
+          name="nombre"
+          value={userData.nombre}
+          onChange={handleChange}
+        />
 
-                <label className='my-1' htmlFor="Teléfono">Teléfono: </label><input className='   w-80 rounded-lg p-1  border-gray-400   border-2 ' type="text" name="Teléfono" />
+        <label className='my-1' htmlFor="contrasenia">
+          Contraseña:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="password"
+          name="contrasenia"
+          value={userData.contrasenia}
+          onChange={handleChange}
+        />
 
-                <label className='my-1' htmlFor="Informacion adicional">Informacion adicional: </label><input className='   w-80 rounded-lg p-1  border-gray-400   border-2 ' type="text" name="Informacion adicional" />
+        <label className='my-1' htmlFor="perfil">
+          Perfil:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="text"
+          name="perfil"
+          value={userData.perfil}
+          onChange={handleChange}
+        />
 
-                <div className=' flex justify-center items-center mt-6 gap-4'>
-                    <button className=' border-[#2794E3] p-2 rounded-2xl px-8 border text-[#2794E3] hover:bg-[#2794E3]  hover:text-white duration-300 '>Cancelar</button>
-                    <button className=' bg-[#2794E3] border p-2 px-10 text-white rounded-2xl hover:bg-white hover:border-[#2794E3]  hover:text-[#2794E3] duration-300'>{" "} Enviar {" "}</button>
-                </div>
+        <label className='my-1' htmlFor="telefono">
+          Teléfono:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="text"
+          name="telefono"
+          value={userData.telefono}
+          onChange={handleChange}
+        />
 
-            </form>
+        <label className='my-1' htmlFor="infoAdicional">
+          Información adicional:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="text"
+          name="infoAdicional"
+          value={userData.infoAdicional}
+          onChange={handleChange}
+        />
+
+        <label className='my-1' htmlFor="estatus">
+          Estatus:
+        </label>
+        <input
+          className='w-80 rounded-lg p-1 border-gray-400 border-2'
+          type="text"
+          name="estatus"
+          value={userData.estatus}
+          onChange={handleChange}
+        />
+
+        <div className='flex justify-center items-center mt-6 gap-4'>
+          <button
+            className='border-[#2794E3] p-2 rounded-2xl px-8 border text-[#2794E3] hover:bg-[#2794E3] hover:text-white duration-300'
+            type="submit"
+          >
+            Cancelar
+          </button>
+          <button
+            className='bg-[#2794E3] border p-2 px-10 text-white rounded-2xl hover:bg-white hover-border-[#2794E3] hover:text-[#2794E3] duration-300'
+            type="submit"
+          >
+            Enviar
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
-export default FormAnadirTrabajadores
+export default FormAnadirTrabajadores;
--- src/Components/Admin/CrudTrabajadores.jsx
+++ src/Components/Admin/CrudTrabajadores.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function CrudTrabajadores() {
-    const [usuarios, setUsuarios] = useState([
-        {
-            userid: 1,
-            username: 'Usuario1',
-            userlevel: 'Nivel 1',
-            enable: true,
-        },
-        {
-            userid: 2,
-            username: 'Usuario2',
-            userlevel: 'Nivel 2',
-            enable: false,
-        },
-        // Agrega más usuarios según sea necesario
-    ]);
+    const [usuarios, setUsuarios] = useState([]);
+    const [deletedUserId, setDeletedUserId] = useState(null);
 
+    useEffect(() => {
+        // Función para obtener la lista de usuarios
+        const fetchUsuarios = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/usuarios'); // Reemplaza la URL con la ruta correcta de tu endpoint de API
+                setUsuarios(response.data);
+            } catch (error) {
+                console.error('Error al obtener usuarios:', error);
+            }
+        };
+
+        fetchUsuarios();
+    }, [deletedUserId]);
+
+    // Función para eliminar un usuario
+    const handleDeleteUsuario = async (userId) => {
+        try {
+            const response = await axios.delete(`http://localhost:3000/usuario/${userId}`); // Reemplaza la URL con la ruta correcta de tu endpoint de eliminación de usuarios
+            if (response.status === 200) {
+                // Marcar el usuario como eliminado
+                setDeletedUserId(userId);
+            }
+        } catch (error) {
+            console.error('Error al eliminar usuario:', error);
+        }
+    };
     return (
         <div className='h-[80vh] mt-10'>
             <div className='flex justify-around'>
@@ -31,35 +45,40 @@ function CrudTrabajadores() {
 
             <div className='grid place-content-center mt-10'>
                 <div>
-                    <table className='w-full border-collapse border border-[#AEC5FF] '>
-                        <thead className='bg-[#AEC5FF] text-white w-2'>
-                            <tr>
-                                <th className='   sm:w-32  text-white p-2' >ID</th>
-                                <th className='   sm:w-32  text-white p-2' >Username</th>
-                                <th className='   sm:w-32  text-white p-2' >User Level</th>
-                                <th className='   sm:w-32  text-white p-2' >Enable</th>
-                                <th className='   sm:w-96  text-white p-2' >Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {usuarios.map((user) => (
-                                <tr key={user.userid}>
-                                    <td className='border border-gray-300 p-2'>{user.userid}</td>
-                                    <td className='border border-gray-300 p-2'>{user.username}</td>
-                                    <td className='border border-gray-300 p-2'>{user.userlevel}</td>
-                                    <td className='border border-gray-300 p-2'>{user.enable ? 'Yes' : 'No'}</td>
-                                    <td className='border border-gray-300 p-2'>
-                                        <div className='flex justify-center max-sm:flex-col'>
-                                            <button className='bg-blue-500 text-white px-2 py-1 mr-2 rounded'>Edit</button>
-                                            <button className='bg-red-500 text-white px-2 py-1 rounded'>Delete</button>
 
-                                        </div>
+                    {usuarios.length > 0 ?
+                        (<>
+                            <table className='w-full border-collapse border border-[#AEC5FF] '>
+                                <thead className='bg-[#AEC5FF] text-white w-2'>
+                                    <tr>
+                                        <th className='sm:w-32 text-white p-2'>ID</th>
+                                        <th className='sm:w-32 text-white p-2'>Username</th>
+                                        <th className='sm:w-32 text-white p-2'>User Level</th>
+                                        <th className='sm:w-32 text-white p-2'>Enable</th>
+                                        <th className='sm:w-96 text-white p-2'>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {usuarios.map((user) => (
+                                        <tr key={user.idUser}>
+                                            <td className='border border-gray-300 p-2'>{user.idUser}</td>
+                                            <td className='border border-gray-300 p-2'>{user.nombre}</td>
+                                            <td className='border border-gray-300 p-2'>{user.perfil}</td>
+                                            <td className='border border-gray-300 p-2'>{user.estatus ? 'Yes' : 'No'}</td>
+                                            <td className='border border-gray-300 p-2'>
+                                                <button className='bg-red-500 text-white px-2 py-1 rounded' onClick={() => handleDeleteUsuario(user.idUser)}>Delete</button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </>)
+                        :
+                        (<>
+                            <div className=' text-4xl font-bold   text-center '>No hay registros de usuarios.</div>
+                        </>)
+                    }
 
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </div>
