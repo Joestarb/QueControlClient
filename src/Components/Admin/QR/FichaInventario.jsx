728727cc22bb71194665acf3807715f35310c97e
Octavio Cruz <dorersempai@gmil.com>
2023-11-06 17:38:53 -0500

    print del qr

--- src/Components/Admin/QR/FichaInventario.jsx
+++ src/Components/Admin/QR/FichaInventario.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import Qr from './Qr';
+import ReactToPrint from 'react-to-print';
 
-import Qr from './Qr'
-
-function FichaInventario({link}) {
+function FichaInventario({ link }) {
   const data = link;
+  const componentRef = React.useRef();
 
-  useEffect(()=>{
-      if(link !== 'http://localhost:5173/producto/'){
-        console.log(data)
-      }
-  },[link])
+  useEffect(() => {
+    if (link !== 'http://localhost:5173/producto/') {
+      console.log(data);
+    }
+  }, [link]);
 
   return (
     <div className='grid place-content-center'>
       <div className='flex justify-center'>
         <div className="App">
-        
-        {link !== 'http://localhost:5173/producto/' ? (
-          <div>
-            {console.log(data)}
-            <Qr data={data} />
-          </div>
-        ) : (
-          <div>
-            <h2>Genera un QR con el form y aparecerá acá</h2>
-          </div>
-)}
-
+          {link !== 'http://localhost:5173/producto/' ? (
+            <div className='flex flex-col'>
+              {console.log(data)}
+              <Qr data={data} ref={componentRef} />
+              <ReactToPrint
+                trigger={() => (
+                  <button className="border-2 rounded-md border-[#2794E3] text-[#2794E3]  m-4 px-2 ">Imprimir codigo QR</button>
+                )}
+                content={() => componentRef.current}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Genera un QR con el form y aparecerá acá</h2>
+            </div>
+          )}
         </div>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default FichaInventario
+export default FichaInventario;
